fc0f2b40c17fc427d3aed45e8edec584e2599b18
nanilzn <some@mail.com>
2020-04-22 06:20:36 +0300

    refactored range-slider, optimized jQuery calls;
     slider doesnt accept anymore small intermidiate values like 4001, 4567 and so on. Only 4000, 4100, ..;

--- src/bem/range-slider/range-slider.js
+++ src/bem/range-slider/range-slider.js
@@ -10,72 +10,91 @@ class RangeSlider
       // in percents
       this.firstControlValue = 100 / 3;
       this.secondControlValue = 100 / 3 * 2;
-      this.minimumValue = 0;
+      this.minimumValue = 1000;
       this.maximumValue = 15000;
+
+      // slider accepts 4000, 4100, 4200 values;
+      // declines 4001, 4256 and so on. I hope you get it
+      this.stepValue = 100;
 
       // in percents
       this.dragMinX = 0;
       this.dragMaxX = 0;
       this.minimumDistanceBetweenSliderControls = 8;
       
-      this.$component.on('mousedown', (ev) => {
-         let eventX = ev.pageX - this.$component.offset().left;
+      // I guess it would save some milliseconds
+      this.startControl = $('.range-slider__start', this.$component);
+      this.endControl = $('.range-slider__end', this.$component);
+      this.filledLine = $('.range-slider__filled-line', this.$component);
+      this.description = $('.range-slider__description', this.$component);
+
+      // mouse events
+      this.$component.on('mousedown', this.onMouseDown.bind(this));
+      $(document).on('mousemove', this.onMouseMove.bind(this));
+      $(document).on('mouseup', this.onMouseUp.bind(this));
+   }
+
+   onMouseDown(ev) {
 
          // in pixels
-         let startX = parseFloat($('.range-slider__start', this.$component).css('left'))
-         let endX   = parseFloat($('.range-slider__end', this.$component).css('left'))
+         let eventX = ev.pageX - this.$component.offset().left;
+         let startX = parseFloat(this.startControl.css('left'))
+         let endX   = parseFloat(this.endControl.css('left'))
 
          if(Math.abs(startX - eventX) > Math.abs(endX - eventX)) {
             this.dragMinX = startX / this.$component.width() * 100 + this.minimumDistanceBetweenSliderControls;
             this.dragMaxX = 100;
-            this.dragComponent = $('.range-slider__end', this.$component);
+            this.dragComponent = this.endControl;
             this.dragType = 'end';
          }
          else {
             this.dragMinX = 0;
             this.dragMaxX = endX / this.$component.width() * 100 - this.minimumDistanceBetweenSliderControls;
-            this.dragComponent = $('.range-slider__start', this.$component);
+            this.dragComponent = this.startControl;
             this.dragType = 'start';
          }
-      });
-      
-      $(document).on('mousemove', (ev) => {
-         if(this.dragComponent != null) {
-            let relX = event.pageX - this.$component.offset().left;
-            let relY = event.pageY - this.$component.offset().top;
+   }
 
-            let val = relX / this.$component.width() * 100;
-            if(val < this.dragMinX)
-               val = this.dragMinX;
-            else if(val > this.dragMaxX)
-               val = this.dragMaxX;
+   onMouseMove(ev) {
+      if(this.dragComponent != null) {
 
-            if(this.dragType == 'start') {
-               this.firstControlValue = val;
+         // in pixels
+         let relX = event.pageX - this.$component.offset().left;
+         let relY = event.pageY - this.$component.offset().top;
 
-               let width = (this.dragMaxX - val + this.minimumDistanceBetweenSliderControls);
-               $('.range-slider__filled-line', this.$component).css('margin-left', `calc(${val}%)`);
-               $('.range-slider__filled-line', this.$component).css('width', `calc(${width}%)`);
-            } else { // this.dragType == 'end'
-               this.secondControlValue = val;
+         // in percents
+         let val = relX / this.$component.width() * 100;
+         if(val < this.dragMinX)
+            val = this.dragMinX;
+         else if(val > this.dragMaxX)
+            val = this.dragMaxX;
 
-               let width = (val - this.dragMinX + this.minimumDistanceBetweenSliderControls);
-               $('.range-slider__filled-line', this.$component).css('width', `calc(${width}%)`);
-            }
+         if(this.dragType == 'start') {
+            this.firstControlValue = val;
 
-            this.dragComponent.css('left', `calc(${val}%)`);
+            let width = (this.dragMaxX - val + this.minimumDistanceBetweenSliderControls);
+            this.filledLine.css('margin-left', `calc(${val}%)`);
+            this.filledLine.css('width', `calc(${width}%)`);
+         } else { // this.dragType == 'end'
+            this.secondControlValue = val;
 
-            // update values
-
-            let range = this.maximumValue - this.minimumValue;
-            $('.range-slider__description', this.$component)
-               .html(`${(range / 100 * this.firstControlValue).toFixed()}₽ - ${(range / 100 * this.secondControlValue).toFixed()}₽`);
+            let width = (val - this.dragMinX + this.minimumDistanceBetweenSliderControls);
+            this.filledLine.css('width', `calc(${width}%)`);
          }
-      });
 
-      $(document).on('mouseup', (ev) => {
-         this.dragComponent = null;
-      });
+         this.dragComponent.css('left', `calc(${val}%)`);
+
+         // update values
+
+         const range = this.maximumValue - this.minimumValue;
+         const minimumDescriptionValue = Math.round((range / 100 * this.firstControlValue + this.minimumValue) / this.stepValue) * this.stepValue;
+         const maximumDescriptionValue = Math.round((range / 100 * this.secondControlValue + this.minimumValue) / this.stepValue) * this.stepValue;
+         this.description.html(`${(minimumDescriptionValue).toFixed()}₽ - ${(maximumDescriptionValue).toFixed()}₽`);
+      }
+   }
+
+   onMouseUp(ev) {
+      this.dragComponent = null;
    }
 }
 

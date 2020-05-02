f26c1223b9e4fd5782c78483886f2ad89257040b
nanilzn <some@mail.com>
2020-05-03 01:21:25 +0300

    calendar in dev;

--- src/bem/calendar/calendar.js
+++ src/bem/calendar/calendar.js
@@ -1,30 +1,65 @@
-import 'air-datepicker/dist/css/datepicker.css';
-import './calendar.scss';
 import 'air-datepicker';
 
+class Calendar
+{
+   constructor($component)
+   {
+      this.$component = $component;
+      
+      let datePicker = $($component).datepicker({
+         inline: true,
+         range: true,
+         multipleDatesSeparator: '-',
+         clearButton: true,
+         todayButton: true,
+         showOtherYears: false,
+         navTitles: {
+            days: 'MM <i>yyyy</i>',
+            months: 'yyyy',
+            years: 'yyyy1 - yyyy2'
+         },
+
+         onRenderCell: function(date, cellType) {
+
+            if(cellType == 'day') {
+            
+               let d = date.getDate(),
+                   m = date.getMonth(),
+                   y = date.getFullYear();
+
+               let isSpecial = false;
+
+               if(d == 8 && m == 7 && y == 2019)
+                  isSpecial = true;
+
+               return {
+                  html: `<div class="calendar__cell-content">${date.getDate()}</div>`,
+                  classes: isSpecial ? 'calendar__cell_special' : 'calendar__cell'
+               }
+            }
+         },
+      }).data('datepicker');
+
+      $($('.datepicker--button', $component)[0]).html('применить');
+
+      datePicker.selectDate(new Date(2019, 7, 19));
+      datePicker.selectDate(new Date(2019, 7, 23));
+
+      // -haxx- zone
+      // If air-datepicker updates one day,
+      // following code might become defective
+
+      datePicker.today = () => {
+         datePicker.hide();
+      };
+
+      datePicker.nav.d.$nav.on('click', '.datepicker--nav-title', () => {});
+      datePicker.nav.d.$nav.prop('click', null);
+   }
+}
+
 $(function() {
-
-   let $start = $('#start'),
-       $end   = $('#end');
-
-   let picker = $('#start').datepicker({
-      range: true,
-     multipleDatesSeparator: '-',
-     language: "en",
-      clearButton: true,
-      todayButton: true,
-      classes: 'abs',
-
-     onSelect: function (fd, d, picker) { 
-       $("#start").val(fd.split("-")[0]);
-       $("#end").val(fd.split("-")[1]);
-     },
-      onHide: function() {
-         console.log(this);
-     }
-   }).data('datepicker');
-
-   $('#end').on('click', () => {
-      picker.show();
+   $('.calendar_inline').each((index, node) => {
+      return new Calendar(node);
    });
 });
